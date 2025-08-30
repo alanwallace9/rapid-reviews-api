ece433d17a6c7b3946c72da00dcb285e67d47a59
alanwallace9 <57368739+alanwallace9@users.noreply.github.com>
2025-08-30 11:24:49 -0500

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -1,6 +1,10 @@
+// api/[...path].js
 import app from '../src/app.js';
 
-// Express apps are request handlers, so just delegate:
 export default function handler(req, res) {
+  // Make Express see "/db-check" instead of "/api/db-check"
+  if (req.url.startsWith('/api')) {
+    req.url = req.url.slice(4) || '/';
+  }
   return app(req, res);
 }
