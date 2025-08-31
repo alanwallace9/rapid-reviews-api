df992bd40c44d80244b1b833c2128f12991c7716
alanwallace9 <57368739+alanwallace9@users.noreply.github.com>
2025-08-31 12:50:02 -0500

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -1,8 +1,7 @@
-// api/[...path].js
+// Serverless entry. No URL rewrites.
 export const config = { runtime: 'nodejs' };
 import app from '../src/app.js';
 
 export default function handler(req, res) {
-  // No URL rewriting. Express will see /api/...
   return app(req, res);
 }
